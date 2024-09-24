8a7ccb2fcf0bae4a66840897f4b7f5b7b714148a
daniel <dcho2001@gmail.com>
2024-09-23 23:46:37 -0400

    add database connection

--- config.php
+++ config.php
@@ -1,14 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";  // Use your own MySQL password, or leave blank if default
-$dbname = "country_searches";
+// Get the DATABASE_URL from Heroku environment variables
+$db = parse_url(getenv("postgres://ua60mjac0k7a4b:pa526b469a3cc8a5d08dd59171c895c666d76320c1a9c61441195f57f7c909991@c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d364glsdk3i6cn"));
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Store the database connection details in variables
+$host = $db["host"];
+$user = $db["user"];
+$password = $db["pass"];
+$dbname = ltrim($db["path"], "/");
+$port = $db["port"];
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Connect to the PostgreSQL database using the connection details
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
 }
 ?>
