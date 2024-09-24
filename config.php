8701f2d1a50dcb35a3d139c1be261b28396925a7
daniel <dcho2001@gmail.com>
2024-09-24 10:41:45 -0400

    Fix database connection

--- config.php
+++ config.php
@@ -1,17 +1,22 @@
 <?php
-// Parse the ClearDB connection information from the environment variable
-$cleardb_url = parse_url("mysql://bbf9f52b4bd161:1731f103@us-cluster-east-01.k8s.cleardb.net/heroku_bcd9c36422ff993?reconnect=true");
+// Parse ClearDB connection information from the environment variable
+$cleardb_url = getenv('CLEARDB_DATABASE_URL');
+if ($cleardb_url) {
+    $cleardb_url_parts = parse_url($cleardb_url);
 
-$host = $cleardb_url["host"]; // us-cluster-east-01.k8s.cleardb.net
-$user = $cleardb_url["user"]; // bbf9f52b4bd161
-$pass = $cleardb_url["pass"]; // 1731f103
-$db = substr($cleardb_url["path"], 1); // heroku_bcd9c36422ff993
+    $host = $cleardb_url_parts["host"];
+    $user = $cleardb_url_parts["user"];
+    $pass = $cleardb_url_parts["pass"];
+    $db   = substr($cleardb_url_parts["path"], 1);
 
-// Create a connection to the database
-$conn = new mysqli($host, $user, $pass, $db);
+    // Create connection
+    $conn = new mysqli($host, $user, $pass, $db);
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+} else {
+    die("CLEARDB_DATABASE_URL is not set");
 }
 ?>
