24f4cc2a88bfd785514ae2f41717c5d2403483df
daniel <dcho2001@gmail.com>
2024-10-01 21:59:52 -0400

    updated styling

--- index.php
+++ index.php
@@ -431,7 +431,7 @@ function convertUTCtoLocal() {
             const formattedDateString = `${weekday}, ${month} ${ordinalDay}, ${year} at ${timeString}`;
 
             // Update the content of the element with the country and formatted local time
-            recentSearchElement.innerText = `Someone searched for ${countrySearched} on ${formattedDateString}`;
+            recentSearchElement.innerText = `Someone searched for ${countrySearched} on ${formattedDateString}.`;
         } else {
             recentSearchElement.innerText = "N/A";  // If invalid, show N/A
         }
