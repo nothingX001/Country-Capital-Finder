37bf6e1e4fb0a5661f261a2a9a036723d278b149
daniel <dcho2001@gmail.com>
2024-10-11 18:22:47 -0400

    new countries

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
 <nav class="navbar">
     <ul class="navbar-list">
         <li><a href="index.php">Home</a></li>
-        <li><a href="country-profile.php">Countries</a></li>
+        <li><a href="country-profiles.php">Countries</a></li>
         <li><a href="quiz.php">Quiz</a></li>
         <li><a href="world-map.php">World Map</a></li>
     </ul>
