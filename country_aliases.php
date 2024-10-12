ed928e19cb86dbfefa3cf90507165bcd8d2de732
daniel <dcho2001@gmail.com>
2024-10-11 21:14:38 -0400

    propaganda

---
+++ country_aliases.php
@@ -0,0 +1,10 @@
+<?php
+$country_aliases = [
+    "USA" => "United States",
+    "US" => "United States",
+    "America" => "United States",
+    "U.S.A." => "United States",
+    "UK" => "United Kingdom",
+    "Britain" => "United Kingdom",
+    // Add more country aliases as needed
+];
