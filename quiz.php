0d0713b5d17d02cc8b08cd6dd03bb5808b86099f
daniel <dcho2001@gmail.com>
2024-10-12 12:47:24 -0400

    FEW

--- quiz.php
+++ quiz.php
@@ -65,7 +65,6 @@ $alias_map = array_merge($country_aliases, $capital_aliases);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Country Capital Quiz</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="quiz-styles.css">
