bd0bacf13309bd9c2fe08b930942787c2de95795
daniel <dcho2001@gmail.com>
2024-10-11 21:34:18 -0400

    terrible

--- navbar.php
+++ navbar.php
@@ -1,9 +1,22 @@
 <!-- navbar.php -->
 <nav class="navbar">
-    <ul class="navbar-list">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="country-profiles.php">Countries</a></li>
-        <li><a href="quiz.php">Quiz</a></li>
-        <li><a href="world-map.php">World Map</a></li>
-    </ul>
+    <div class="navbar-container">
+        <a href="index.php" class="navbar-logo">Home</a>
+        <button class="navbar-toggle" id="navbarToggle" aria-label="Toggle navigation">
+            &#9776; <!-- Hamburger icon -->
+        </button>
+        <ul class="navbar-list" id="navbarList">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="country-profiles.php">Country Profiles</a></li>
+            <li><a href="quiz.php">Quiz</a></li>
+            <li><a href="world-map.php">World Map</a></li>
+        </ul>
+    </div>
 </nav>
+
+<script>
+// Toggle the visibility of the navbar list on mobile
+document.getElementById('navbarToggle').addEventListener('click', function() {
+    document.getElementById('navbarList').classList.toggle('open');
+});
+</script>
