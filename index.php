79a6ef80f4c308d024e9b5a7da84938fdef37cc1
daniel <dcho2001@gmail.com>
2025-01-20 01:38:46 -0500

    remove styles

--- index.php
+++ index.php
@@ -90,9 +90,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <?php include 'navbar.php'; ?>
 
     <div class="main">
-        <h1>CAPITAL FINDER</h1>
+        <h1>Find the capital of any country: </h1>
         <form action="index.php" method="post">
-            <label><h3>Find the capital of any country: </h3></label>
+            <label><h3>Search</h3></label>
             <input type="text" name="country" autocomplete="off" required>
             <input type="submit" value="SUBMIT">
         </form>
