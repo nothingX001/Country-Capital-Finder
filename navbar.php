371e83a3ed93953873527f85e11f37f7ff2727f0
daniel <dcho2001@gmail.com>
2025-03-02 14:18:29 -0500

    style

--- navbar.php
+++ navbar.php
@@ -1,15 +1,19 @@
 <!-- navbar.php -->
 <nav class="navbar">
-    <div class="navbar-container">
-        <button class="navbar-toggle" id="navbarToggle" aria-label="Toggle navigation"></button>
-        <ul class="navbar-list" id="navbarList">
-            <li><a href="index.php">HOME</a></li>
-            <li><a href="country-profiles.php">COUNTRY PROFILES</a></li>
-            <li><a href="quiz.php">QUIZ</a></li>
-            <li><a href="world-map.php">WORLD MAP</a></li>
-            <li><a href="about.php">ABOUT</a></li>
-        </ul>
+  <div class="navbar-container">
+    <button class="navbar-toggle" id="navbarToggle" aria-label="Toggle navigation"></button>
+    <!-- Centered logo -->
+    <div class="navbar-logo">
+      <img src="images/explore-capitals-logo.jpg" alt="ExploreCapitals Logo">
     </div>
+    <ul class="navbar-list" id="navbarList">
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="country-profiles.php">COUNTRY PROFILES</a></li>
+      <li><a href="quiz.php">QUIZ</a></li>
+      <li><a href="world-map.php">WORLD MAP</a></li>
+      <li><a href="about.php">ABOUT</a></li>
+    </ul>
+  </div>
 </nav>
 <script>
 document.getElementById('navbarToggle').addEventListener('click', function() {
